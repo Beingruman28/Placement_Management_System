8af5d574581a4eb51129a1ce91860d10918717c6
ruman <mohammedruman1996@gmail.com>
2022-04-13 22:10:29 +0530

    new complete updated project of placement management system..

--- Placement_Management_System/src/com/cg/placement/service/ICollegeService.java
+++ Placement_Management_System/src/com/cg/placement/service/ICollegeService.java
@@ -1,13 +1,14 @@
 package com.cg.placement.service;
 
 import com.cg.placement.entities.College;
+import com.cg.placement.entities.Placement;
 
 public interface ICollegeService 
 {
 	public College addCollege(College college);
 	public College updateCollege(College college);
-	public College searchCollege(College college);
+	public College searchCollegeById(int id);
 	public boolean deleteCollege(int id);
-	public College schedulePlacement(College placement);
-	
+	public boolean schedulePlacement(Placement placement);
+		
 }
